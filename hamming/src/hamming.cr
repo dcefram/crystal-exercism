c2826666bde53f1f3bdfc600e80995893df30e29
Daniel Cefram Ramirez <dcefram@outlook.com>
2019-11-12 08:20:56 +0800

    track 2: update code to idiomatic crystal

--- hamming/src/hamming.cr
+++ hamming/src/hamming.cr
@@ -1,16 +1,9 @@
 module Hamming
-  def self.distance(firstStrand : String, secondStrand : String)
-    if (firstStrand.chars.size != secondStrand.chars.size)
+  def self.distance(first_strand : String, second_strand : String)
+    if (first_strand.size != second_strand.size)
       raise ArgumentError.new("Strands should be of same length")
     end
 
-    # I can't find the proper way to loop in the docs, aside from using maps...
-    distance = 0
-
-    firstStrand.chars.map_with_index do |char, index|
-      distance = distance + 1 if char != secondStrand.chars[index]
-    end
-
-    distance
+    first_strand.chars.zip(second_strand.chars).count { |item| item[0] != item[1] }
   end
 end
